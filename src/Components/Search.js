350a328e96fd777c0d6dd8a097a73ad2994e857a
shivamcs <shivamcs0049@gmail.com>
2019-06-07 17:00:54 +0530

    installed open iconic

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -29,9 +29,9 @@ class SearchComponent extends React.Component {
     }
 
 
-    componentWillUnmount(){
+    componentWillUnmount() {
         this.props.dispatch({
-            type:'CLEAR_VIDEOS'
+            type: 'CLEAR_VIDEOS'
         })
     }
     render() {
@@ -43,7 +43,9 @@ class SearchComponent extends React.Component {
                     <div className="col">
                         <input type="text" onChange={this.inputChanged} className="form-control form-control-lg" />
                     </div>
-                    <button className="btn btn-info btn-lg" onClick={this.buttonClicked}>Search</button>
+                    <button className="btn btn-danger btn-lg" onClick={this.buttonClicked}>
+                        <span class="oi oi-magnifying-glass"></span>
+                    </button>
                 </div>
                 <Videos />
             </div>
