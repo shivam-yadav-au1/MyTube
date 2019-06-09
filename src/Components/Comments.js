220df89a8b0feb4149d0e7e130bef3343a01e126
shivamcs <shivamcs0049@gmail.com>
2019-06-09 22:51:06 +0530

    video comments rendering done

--- src/Components/Comments.js
+++ src/Components/Comments.js
@@ -1,19 +1,30 @@
 import React from 'react';
-import {connect} from 'react-redux'
-import {stateMapper} from '../Store/Store.js'
-class CommentsComponent extends React.Component{
+import { connect } from 'react-redux'
+import { stateMapper } from '../Store/Store.js'
+class CommentsComponent extends React.Component {
 
-    componentDidMount(){
-        
+    componentDidMount() {
+
         this.props.dispatch({
-            type:"FETCH_VIDEO_COMMENTS",
-            videoId:this.props.videoId
+            type: "FETCH_VIDEO_COMMENTS",
+            videoId: this.props.videoId
         })
     }
 
-    render(){
+    render() {
         return (
-            <p>Comments Loading ....</p>
+            this.props.currentVideoComments.map(c => {
+                return (
+
+                    <p key={c.id}>
+                        <img src={c.snippet.topLevelComment.snippet.authorProfileImageUrl}/>
+                        <strong>{c.snippet.topLevelComment.snippet.authorDisplayName} : says</strong><br/>
+                        {c.snippet.topLevelComment.snippet.textOriginal}
+                        <hr />
+                    </p>
+
+                )
+            })
         )
     }
 }
